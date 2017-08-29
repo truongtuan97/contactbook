67fcb91e86b49deac29fab9eae4d62e134b88abb
Ngoc Tuan Truong <truongtuan97@gmail.com>
2017-08-29 14:10:30 +0700

    try config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+      invoke! 'puma:restart'
     end
   end
 

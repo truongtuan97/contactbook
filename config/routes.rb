bc5d12f1f84c4c1a6d034787a1b4d7763f251454
Ngoc Tuan Truong <truongtuan97@gmail.com>
2017-09-01 11:30:09 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'contacts#index'
 end
